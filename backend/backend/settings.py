8d13b5bcc24b9e3ea20df21e6ce8f0a820da1316
aymeric-nzore <aymeric.nzore2024@esatic.edu.ci>
2025-11-22 12:11:51 +0000

    ajout de requirements.txt

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -8,7 +8,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2#5o)b57d_-$52n60%1ig!vn1xm7jfi8-vim8!s5lb!3kb73mj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
