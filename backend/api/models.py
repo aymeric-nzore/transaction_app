0e50ef99b169ed7f1fd5d3d9259ac1e3c5c2e07e
aymeric-nzore <aymeric.nzore2024@esatic.edu.ci>
2025-11-22 19:00:38 +0000

    Fix created_at to DateTimeField

--- backend/api/models.py
+++ backend/api/models.py
@@ -5,7 +5,7 @@ class Transaction(models.Model):
     id = models.UUIDField(default=uuid.uuid4, editable=False , primary_key=True)
     text = models.CharField(max_length=255)
     amount = models.DecimalField(decimal_places=2 , max_digits=10)
-    created_at = models.DateField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"{self.text} ({self.amount})"
